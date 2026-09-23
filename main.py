0c90945b6ee92085214ae3326cf344471c7b66b8
Denis <5603353@mail.ru>
2026-09-23 13:14:22 +0300

    топ 5 стран ввоздухе

--- main.py
+++ main.py
@@ -14,4 +14,12 @@ if response.status_code == 200:
     country_uniqon = set(country)
     print(len(country_uniqon))
 
-    
+    counts_contry = {}
+    for i in country:
+        counts_contry[i] = counts_contry.get(i, 0) + 1
+    top5 = sorted(counts_contry.items(), key=lambda item:item[1], reverse=True)[:5]
+    b = []
+    for i in top5:
+        b.append(i[0])
+
+    print(b)
